f30f3576255f14f5b54f7e90917927b23a9807fc
Nazar <134242336+barchenkoN@users.noreply.github.com>
2025-12-10 17:34:23 +0200

    Delete Web_Projects/PZ4 directory

--- Web_Projects/PZ4/src/services/UserService.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../types/UserTypes.js";
-
-export async function fetchUsersData(): Promise<User[]> {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    if (!response.ok) {
-        throw new Error('Помилка завантаження');
-    }
-    return await response.json();
-}
